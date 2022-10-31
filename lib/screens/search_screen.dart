a008c9c32f2dc9a4c0a921fcbcf9ecc6324beae0
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-10-30 22:46:07 -0500

    feat: searchScreen - Hotels

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -19,27 +19,41 @@ class SearchScreen extends StatelessWidget {
           Gap(AppLayout.getHeight(40)),
           Text("What are\nyou looking for?", style: Styles.headLineStyle1.copyWith(fontSize: AppLayout.getWidth(35)),),
           Gap(AppLayout.getHeight(20)),
-          Container(
-            padding: const EdgeInsets.all(3.5),
-            child: Row(
-              children: [
-                /*
-              airline tickets
-               */
-                Container(
-                  width: size.width * 0.44,
-                  padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.horizontal(left: Radius.circular(AppLayout.getHeight(50))),
-                      color: Colors.white
+          FittedBox(
+            child: Container(
+              padding: const EdgeInsets.all(3.5),
+              child: Row(
+                children: [
+                  /*
+                airline tickets
+                 */
+                  Container(
+                    width: size.width * 0.44,
+                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.horizontal(left: Radius.circular(AppLayout.getHeight(50))),
+                        color: Colors.white
+                    ),
+                    child: Center(child: Text("Airline tickets")),
                   ),
-                  child: Center(child: Text("Airline tickets")),
-                )
-              ],
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(AppLayout.getHeight(50),),
-              color: const Color(0xFFF4F6FD)
+                  /*
+                hotels
+                 */
+                  Container(
+                    width: size.width * 0.44,
+                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.horizontal(right: Radius.circular(AppLayout.getHeight(50))),
+                        color: Colors.transparent
+                    ),
+                    child: Center(child: Text("Hotels")),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(AppLayout.getHeight(50),),
+                color: const Color(0xFFF4F6FD)
+              ),
             ),
           )
         ],
