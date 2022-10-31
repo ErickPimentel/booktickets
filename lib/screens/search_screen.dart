511dffed41315fad7e8de805b2fc589fcab90677
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-10-30 22:42:08 -0500

    feat: seachScreen - Airline tickets

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -10,6 +10,7 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
@@ -18,6 +19,29 @@ class SearchScreen extends StatelessWidget {
           Gap(AppLayout.getHeight(40)),
           Text("What are\nyou looking for?", style: Styles.headLineStyle1.copyWith(fontSize: AppLayout.getWidth(35)),),
           Gap(AppLayout.getHeight(20)),
+          Container(
+            padding: const EdgeInsets.all(3.5),
+            child: Row(
+              children: [
+                /*
+              airline tickets
+               */
+                Container(
+                  width: size.width * 0.44,
+                  padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(left: Radius.circular(AppLayout.getHeight(50))),
+                      color: Colors.white
+                  ),
+                  child: Center(child: Text("Airline tickets")),
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(AppLayout.getHeight(50),),
+              color: const Color(0xFFF4F6FD)
+            ),
+          )
         ],
       ),
     );
