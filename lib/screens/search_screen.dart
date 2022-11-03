d9bc633a741b0bc5c85b53dbf55d56eceb0722e6
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-11-03 00:36:33 -0500

    feat: first image and text from search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -78,7 +78,50 @@ class SearchScreen extends StatelessWidget {
               ),
             )
           ),
-          const AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View all")
+          Gap(AppLayout.getHeight(40)),
+          const AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View all"),
+          Gap(AppLayout.getHeight(15)),
+          Row(
+            children: [
+              Container(
+                height: AppLayout.getHeight(400),
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15), vertical: AppLayout.getWidth(15)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      blurRadius: 1,
+                      spreadRadius: 1
+                    )
+                  ]
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(AppLayout.getHeight(12)),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                            image: AssetImage(
+                              "assets/images/sit.jpg"
+                            )
+                        )
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Text(
+                      "20% discount on the early booking of this flight. Don't miss.",
+                      style: Styles.headLineStyle2,
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
